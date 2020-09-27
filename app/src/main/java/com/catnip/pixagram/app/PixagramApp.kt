c5b860f77490489106eb8152d737afa32f65949b
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2020-09-28 01:24:11 +0700

    integrate core module in app and add koin in module app

---
+++ app/src/main/java/com/catnip/pixagram/app/PixagramApp.kt
@@ -0,0 +1,35 @@
+package com.catnip.pixagram.app
+
+import android.app.Application
+import com.catnip.core.di.databaseModule
+import com.catnip.core.di.networkModule
+import com.catnip.core.di.repositoryModule
+import com.catnip.pixagram.di.useCaseModule
+import com.catnip.pixagram.di.viewModelModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+class PixagramApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@PixagramApp)
+            modules(
+                listOf(
+                    databaseModule,
+                    networkModule,
+                    repositoryModule,
+                    useCaseModule,
+                    viewModelModule
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/catnip/pixagram/di/AppModule.kt
@@ -0,0 +1,14 @@
+package com.catnip.pixagram.di
+
+import org.koin.dsl.module
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+
+val useCaseModule = module {
+}
+
+val viewModelModule = module {
+}
