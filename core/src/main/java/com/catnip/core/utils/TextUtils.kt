c80fa621846a92542c44fc229328c1518d9915dd
Muhammad Hermas Yuda Pamungkas <hermas.yuda@gmail.com>
2020-09-29 01:07:37 +0700

    creating adapter for feeds

---
+++ core/src/main/java/com/catnip/core/utils/TextUtils.kt
@@ -0,0 +1,11 @@
+package com.catnip.core.utils
+
+/**
+Written with love by Muhammad Hermas Yuda Pamungkas
+Github : https://github.com/hermasyp
+ **/
+object TextUtils {
+    fun getTags(tag : String) : String{
+        return tag.split(",").joinToString (separator = "#")
+    }
+}
